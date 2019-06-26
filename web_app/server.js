76d951414dd51ff270e7db1a52f04398bd7b8063
Andrei Zuica <zuica.andrei@gmail.com>
2019-06-26 18:17:31 +0300

    - Added chart in web app

--- web_app/server.js
+++ web_app/server.js
@@ -7,6 +7,10 @@ server.get('/', function (req, res) {
     res.render('index');
 });
 
+server.get('/chart', function (req, res) {
+    res.render('chart');
+});
+
 server.listen(3000, function () {
     console.log('Listening on port 3000!')
 });
